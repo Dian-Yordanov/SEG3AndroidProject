4e83e020df9fd875771d6d17236c0b621c8051f3
Xynnect <dido.jordanov7@gmail.com>
2014-02-11 17:31:15 +0000

    created the ugly first logical version of the code

--- SEG3/src/com/example/seg3/Main.java
+++ SEG3/src/com/example/seg3/Main.java
@@ -2,22 +2,30 @@ package com.example.seg3;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
-
+	Button withAppointment;
+	Button withoutAppointment;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        withAppointment = (Button) findViewById(R.id.withAppointment);
+        withoutAppointment = (Button) findViewById(R.id.withoutAppointment);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    
+    public void gotoWithAppointmentView(View view) {
+		Intent i = new Intent(Main.this,withAppointment.class);
+		startActivity(i);
+	}
+    public void gotoWithoutAppointmentView(View view) {
+		Intent i = new Intent(Main.this,withoutAppointment.class);
+		startActivity(i);
+	}
     
 }
---
+++ SEG3/src/com/example/seg3/withoutAppointment.java
@@ -0,0 +1,14 @@
+package com.example.seg3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class withoutAppointment extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.without_appointment);
+    }
+
+}
