0253cf9c185b3a11c46d102d3922bf1c9d9232ab
Xynnect <dido.jordanov7@gmail.com>
2014-02-12 16:04:32 +0000

    inflator class improvement

--- SEG3/src/com/example/seg3/questionWithOpenTextViewAnswer.java
+++ SEG3/src/com/example/seg3/questionWithOpenTextViewAnswer.java
@@ -1,22 +1,30 @@
 package com.example.seg3;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class questionWithOpenTextViewAnswer extends LinearLayout{
 	
-	public View view;
+	public static View inflatedView;
+	public static LayoutInflater questionWithOpenTextAnswer;
+	TextView question;
+	EditText answer;
+	LinearLayout mainLayout;
 	
 	public questionWithOpenTextViewAnswer(Context context) {
 		super(context);
 		
 	}
-	public static void fillInflator(View view, String questionText, String answer){
+
+	public View inflator(View view, String questionText, String answer){
+		questionWithOpenTextAnswer = LayoutInflater.from(getContext());
+		return inflatedView = questionWithOpenTextAnswer.inflate(R.layout.question_with_open_text_answer_layout, 
+				mainLayout,false);
 		
 	}
-	
-
-	
 }
