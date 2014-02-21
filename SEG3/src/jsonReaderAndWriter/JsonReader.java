8c910f4cdf39d2b76a1873c0788859f06c5ce477
Xynnect <dido.jordanov7@gmail.com>
2014-02-21 20:53:49 +0000

    added jsonReader and jsonWriter to the main Project and now it is ready for export

---
+++ SEG3/src/jsonReaderAndWriter/JsonReader.java
@@ -0,0 +1,77 @@
+package jsonReaderAndWriter;
+/**
+ * Created by Xelnect on 2/19/14.
+ */
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class JsonReader {
+
+    public static void main(String[] args) {
+        jsonObjectExtractor(jsonFileReader());
+    }
+
+    public static JSONObject jsonFileReader() {
+        JSONParser parser = new JSONParser();
+        Object objToBeParsedFromFile = null;
+        JSONObject jsonObjectFromObj = null;
+
+        try {
+            objToBeParsedFromFile = parser.parse(new FileReader("C:\\Users\\Xelnect\\Desktop\\JSonWritten.json"));
+            jsonObjectFromObj = (JSONObject) objToBeParsedFromFile;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return jsonObjectFromObj;
+    }
+
+    public static void jsonObjectExtractor(JSONObject objToBeParsedFromFile) {
+
+        JSONObject questionNumber = null;
+        for(int i=0;i<objToBeParsedFromFile.size();i++){
+        String questionNumberIndex = "question number ".concat(Integer.toString(i+1));
+
+        questionNumber = (JSONObject) objToBeParsedFromFile.get(questionNumberIndex);
+        System.out.println(questionNumber);
+
+        readQuestionType(questionNumber);
+        readQuestionText(questionNumber);
+        readAnswerText(questionNumber);
+
+        System.out.println( readQuestionType(questionNumber));
+        System.out.println(readQuestionText(questionNumber));
+        System.out.println(readAnswerText(questionNumber));
+        }
+
+    }
+    public static ArrayList readAnswerText(JSONObject obj) {
+        JSONArray answerTextJsonArray = (JSONArray) obj.get("answerText");
+        ArrayList answerTextArray = new ArrayList();
+        Iterator<String> iterator = answerTextJsonArray.iterator();
+        while (iterator.hasNext()) {
+            answerTextArray.add(iterator.next());
+        }
+        return answerTextArray;
+    }
+    public static String readQuestionText(JSONObject obj) {
+        String questionText = (String) obj.get("questionText");
+        return questionText;
+    }
+    public static String readQuestionType(JSONObject obj) {
+        String questionType = (String) obj.get("questionType");
+        return questionType;
+    }
+}
---
+++ SEG3/src/jsonReaderAndWriter/JsonWriter.java
@@ -0,0 +1,77 @@
+package jsonReaderAndWriter;
+/**
+ * Created by Xelnect on 2/19/14.
+ */
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JsonWriter {
+
+    public static JSONObject objMain = new JSONObject();
+    public static int i=0;
+
+    public static void main(String[] args) {
+
+        String questionType1 = "radio button";
+        String questionText1 = "do you have some problems with your hearth?";
+        String[] answerText1 = {"yes", "no"};
+
+        String questionType2 = "check boxes";
+        String questionText2 = "which memebers of your family have heart problems?";
+        String[] answerText2 = {"your mother", "your father", "your uncle", "your aunt"};
+
+
+        writeJsonToFile(questionType1, questionText1, answerText1);
+        writeJsonToFile(questionType2, questionText2, answerText2);
+    }
+
+    public static void questionDetailsJsonObj(String questionType, String questionText, String[] answerText) {
+        i++;
+        JSONArray list = new JSONArray();
+        JSONObject obj = new JSONObject();
+        writeQuestionType(obj, questionType);
+        writeQuestionText(obj, questionText);
+        writeAnswerText(obj, list, answerText);
+
+        objMain.put("question number " + i, obj);
+
+    }
+
+    public static void writeJsonToFile(String questionType, String questionText, String[] answerText) {
+
+        questionDetailsJsonObj(questionType, questionText, answerText);
+
+        try {
+
+            FileWriter file = new FileWriter("C:\\Users\\Xelnect\\Desktop\\JSonWritten.json");
+            file.write(String.valueOf(objMain));
+            file.flush();
+            file.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    public static void writeAnswerText(JSONObject obj, JSONArray list, String[] answerText) {
+        for (int i = 0; i < answerText.length; i++) {
+            list.add(answerText[i]);
+        }
+        obj.put("answerText", list);
+    }
+    public static void writeQuestionText(JSONObject obj, String questionText) {
+        obj.put("questionText", questionText);
+    }
+    public static void writeQuestionType(JSONObject obj, String questionType) {
+        obj.put("questionType", questionType);
+    }
+
+}
